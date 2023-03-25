29df284f45b9454da17ab1328f363968880a6d74
PatAPizza56 <michaelcanudas@gmail.com>
2023-03-25 00:21:47 -0400

    OMG!

--- Uno.Client/Client.cs
+++ Uno.Client/Client.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telepathy;
+
+Client client = new();
+client.Connect("127.0.0.1", 12345);
+
+while (true)
+{
+    while (client.GetNextMessage(out Message msg))
+    {
+        switch (msg.eventType)
+        {
+            case EventType.Connected:
+                OnConnect(msg);
+                break;
+            case EventType.Data:
+                OnData(msg);
+                break;
+            case EventType.Disconnected:
+                OnDisconnect(msg);
+                break;
+        }
+    }
+}
+
+void OnConnect(Message msg)
+{
+    Console.WriteLine("Connected");
+}
+
+void OnData(Message msg)
+{
+    Console.WriteLine("Received: " + msg.data);
+}
+
+void OnDisconnect(Message msg)
+{
+    Console.WriteLine("Disconnected");
+}
--- Uno.Server/Server.cs
+++ Uno.Server/Server.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telepathy;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Server.Instance.Tick();
+        }
+    }
+}
+
+public class Packet
+{
+    public Packet()
+    {
+        Server.Instance.Packets 
+    }
+}
+
+public class Server
+{
+    public static Server Instance { get; } = new();
+    
+    private Telepathy.Server server;
+    
+    public Server()
+    {
+        server = new();
+        server.Start(12345);
+    }
+
+    public void Tick()
+    {
+        while (server.GetNextMessage(out Message msg))
+        {
+            switch (msg.eventType)
+            {
+                case EventType.Connected:
+                    OnConnect(msg);
+                    break;
+                case EventType.Data:
+                    OnData(msg);
+                    break;
+                case EventType.Disconnected:
+                    OnDisconnect(msg);
+                    break;
+            }
+        }
+    }
+
+    private void OnConnect(Message message)
+    {
+        
+    }
+
+    private void OnData(Message message)
+    {
+
+    }
+
+    private void OnDisconnect(Message message)
+    {
+
+    }
+}
